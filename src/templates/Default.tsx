090b891fb71601e7ae63564bd1c31ce611aa2615
STNeto1 <gfreitasneto18@gmail.com>
2023-03-17 18:52:30 -0300

    basic auth visual

---
+++ src/templates/Default.tsx
@@ -0,0 +1,183 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  useAuth,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+import { Button } from "~/components/Button";
+
+const navigation = [{ name: "Home", href: "/", current: true }];
+
+type Props = {};
+
+export const DefaultLayout = (props: PropsWithChildren<Props>) => {
+  const { isLoaded, user } = useUser();
+  const { signOut } = useAuth();
+
+  return (
+    <>
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-8 w-8"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                        alt="Workflow"
+                      />
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex items-baseline space-x-4">
+                        {navigation.map((item) => (
+                          <Link
+                            key={item.name}
+                            href={item.href}
+                            className={clsx(
+                              item.current
+                                ? "bg-gray-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              "rounded-md px-3 py-2 text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-4 flex items-center gap-2 md:ml-6">
+                      <button
+                        type="button"
+                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      >
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+
+                      <SignedIn>
+                        {/* Mount the UserButton component */}
+                        <UserButton />
+                      </SignedIn>
+                      <SignedOut>
+                        {/* Signed out users get sign in button */}
+                        <SignInButton mode="modal">
+                          <Button size={"sm"}>Entrar</Button>
+                        </SignInButton>
+                      </SignedOut>
+                    </div>
+                  </div>
+                  <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <Bars3Icon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="md:hidden">
+                <div className="w-full space-y-1 px-2 pt-2 pb-3 sm:px-3">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={clsx(
+                        item.current
+                          ? "block w-full bg-gray-900 text-center text-white"
+                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "rounded-md px-3 py-2 text-sm font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+                <div className="border-t border-gray-700 pt-4 pb-3">
+                  <SignedIn>
+                    <div className="flex items-center px-5">
+                      <div className="flex-shrink-0">
+                        <Image
+                          className="h-10 w-10 rounded-full"
+                          src={user?.profileImageUrl ?? ""}
+                          width={40}
+                          height={40}
+                          alt=""
+                        />
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-base font-medium leading-none text-white">
+                          {user?.fullName}
+                        </div>
+                        <div className="text-sm font-medium leading-none text-gray-400">
+                          {user?.primaryEmailAddress?.emailAddress}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="mt-6 space-y-1 px-2">
+                      <Button
+                        onClick={() => signOut()}
+                        className="block w-full"
+                        size={"sm"}
+                      >
+                        Sair
+                      </Button>
+                    </div>
+                  </SignedIn>
+
+                  <div className="mt-3 space-y-1 px-2">
+                    <SignedOut>
+                      {/* Signed out users get sign in button */}
+                      <SignInButton mode="modal">
+                        <Button size={"sm"} className={"w-full"}>
+                          Entrar
+                        </Button>
+                      </SignInButton>
+                    </SignedOut>
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        {/* <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+          </div>
+        </header> */}
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {props.children}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
